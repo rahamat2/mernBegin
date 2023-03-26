a325f19b85827fc125dbe4d5095dd1e1ed981eb4
Rahamat-E-Elahi <rahamatelahi4@gmail.com>
2023-03-26 02:45:02 -0700

    Server completed, data insert get done

--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const UserModel = require("./models/Users")
+const UserModel = require("./models/Users");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
     "mongodb+srv://rahamat:mw51V4tU4Bo0Z9xT@cluster0.jph6wxy.mongodb.net/mernF1?retryWrites=true&w=majority"
 );
 
 app.get("/getUsers",(req,res)=>{
-    // UserModel.find({}, (err, result)=>{
-    //     if(err){
-    //         res.json(err);
-    //     }else{
-    //         res.json(result);
-    //     }
-    // });
     UserModel.find({}).then(docs => {
         res.json(docs);
     }).catch(err => {
         res.json(err);
     });
+});
+
+app.post("/createUser", async (req, res)=>{
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+    res.json(user);
 });
 
 app.listen(3001, ()=>{
