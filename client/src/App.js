c6882d3b07b53673ca2d69a2d3e722d68760251c
Rahamat-E-Elahi <rahamatelahi4@gmail.com>
2023-03-29 12:43:12 -0700

    Client done with getuser and createUser

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/getUsers").then((response)=>{
+      setListOfUsers(response.data);
+    });
+  }, []);
+  const createUser = ()=>{
+    Axios.post("http://localhost:3001/createUser", {
+      name, age, username,
+    }).then((response)=>{
+      setListOfUsers([...listOfUsers, {name, age, username,}]);
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input 
+          type="text" 
+          placeholder='Name'
+          onChange={(event)=>{
+            setName(event.target.value);
+          }} 
+        />
+        <input 
+          type="number" 
+          placeholder='Age'
+          onChange={(event)=>{
+            setAge(event.target.value);
+          }} 
+        />
+        <input 
+          type="text" 
+          placeholder='Username' 
+          onChange={(event)=>{
+            setUsername(event.target.value);
+          }}
+        />
+        <button onClick={createUser}>Create User</button>
+      </div>
+      <div className='usersDisplay'>
+        {listOfUsers.map((user)=>{
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h3>Age: {user.age}</h3>
+              <h3>Username: {user.username}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
